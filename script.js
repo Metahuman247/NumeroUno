9c84b5774d579aa51475c42ff53d693c4a2d1b55
Marcus Cochran <mcochran@westinghousehs.org>
2018-05-07 22:22:33 +0000

    Improved

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-function giphyURLWithSearchTerm(searchTerm) {
-}
-
-function appendImageToBody(srcURL) {
-    $('#movies').html('<img src=' + srcURL + '/>');
-    
-}
-
 function callGiphyAPIWithSearchTerm(searchTerm) {
     $.ajax({
       url: "https://www.omdbapi.com/?s="+ searchTerm + "&apikey=5fa6d6c6&page=1",
       method: "GET",
       success: function(response) {
         console.log(response);
-        var url = response.Poster;
-           $("#movies").html('<img src=' + url + '/>');
-           for(i=0; i < response.Search.length; i++){
-               console.log(response);
-               var search = response.Search;
-               $("#movies").append('<img src=' + search[i].Poster + '/>');
-           }
-        // for loop that goes through response.Search
-        // inside the loop you want to grab response.Search[index].Poster as url
-        // $("#movies").html('<img src=' + url + '/>');
+        var search = response.Search;
+        for(i=0; i < response.Search.length; i++){
+           var html = "";
+           html += "<div class='movie-items' data-id="+search[i].imdbID+">";
+           html +='<img src=' + search[i].Poster + '/>';
+           html += "</div>";
+           $("#movies").append(html);
+        }
 
       },
     }); 
-    
-    // $.ajax({
-      //url: "http://www.omdbapi.com/?t="+ searchTermTwo + "&apikey=5fa6d6c6",
-     // method: "GET",
-     // success: function(response) {
-        // Log the whole response to the console
-       // console.log(response);
-        
-        //var urltwo = response.data[0].images.original.url;
-      //  $('#combine').html('<img src=' + urltwo + '/>');
-     // },
-    //}); 
 }
 
 $("#search").click(function(){
@@ -44,6 +22,36 @@ $("#search").click(function(){
     callGiphyAPIWithSearchTerm(inputValue);
 });
 
+$('#search').on('keydown', function(e) {
+    if (e.keyCode === 13) {
+        var inputValue = $("input").val();
+        callGiphyAPIWithSearchTerm(inputValue);
+    }
+});
+
 $("#click").click(function(){
     $("#add").append(input);
 });
+
+$("body").on("click",  ".movie-items",function(e){
+    e.preventDefault();
+    var movieId = $(this).data("id");
+    console.log(movieId);
+    // http://www.omdbapi.com/?i=tt0848228
+     $.ajax({
+      url: "https://www.omdbapi.com/?i=" + movieId + "&apikey=5fa6d6c6",
+      method: "GET",
+      success: function(response) {
+        console.log(response);
+        $("#movieModal").modal({ show: true });
+        $(".modal-title").html(response.Title);
+        $("#poster").attr("src", response.Poster);
+        $("#ratings").html(response.Rated);
+        $("genre").html(response.Genre);
+        $("#plot").html(response.Plot);
+        $("#runtime").html(response.Runtime);
+        $("#year").html(response.Year);
+      }
+    });
+});
+
